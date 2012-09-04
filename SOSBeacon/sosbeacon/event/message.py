0b7cb5864a95f1ac172a42af989b439c75056dca
Robert Kluin <robert.kluin@ezoxsystems.com>
2012-09-04 01:15:08 -0500

    Adding rough version of Message and some unit tests.

---
+++ SOSBeacon/sosbeacon/event/message.py
@@ -0,0 +1,139 @@
+
+from google.appengine.api import taskqueue
+from google.appengine.ext import ndb
+
+import voluptuous
+
+from skel.datastore import EntityBase
+
+GROUP_TX_QUEUE = "group-tx"
+
+message_schema = {
+    'key': voluptuous.any(None, voluptuous.ndbkey(), ''),
+    'event': voluptuous.ndbkey(),
+    'timestamp': basestring,
+    'type': basestring,
+    'message': {
+        'message': basestring,
+        'sms': basestring
+    }
+}
+
+message_query_schema = {
+    'feq_event': voluptuous.ndbkey()
+}
+
+
+class Message(EntityBase):
+    """Represents a message in an event's message stream."""
+    # Store the schema version, to aid in migrations.
+    version_ = ndb.IntegerProperty('v_', default=1)
+
+    event = ndb.KeyProperty('e')
+
+    timestamp = ndb.DateTimeProperty('ts', auto_now_add=True)
+
+    message_type = ndb.StringProperty('mt')
+    message = ndb.JsonProperty('m')
+
+    @classmethod
+    def from_dict(cls, data):
+        """Instantiate a Message entity from a dict of values."""
+        key = data.get("key")
+        message = None
+        if key:
+            message = key.get()
+
+        if not message:
+            from google.appengine.api import namespace_manager
+
+            event_key = data.get('event')
+            if not event_key:
+                # TODO: Raise some other error type here?
+                raise Exception("Event key is required.")
+
+            event = event_key.get()
+            if not event:
+                # TODO: Raise some other error type here?
+                raise Exception("Event not found.")
+
+            if event.school != unicode(namespace_manager.get_namespace()):
+                # TODO: Raise some other error type here?
+                raise Exception("Security violation!")
+
+            message = cls(event=event.key)
+
+        message_type = data.get('type')
+        message.message_type = message_type
+
+        message_data = data.get('message')
+        if message_type == 'c':
+            assert ['body'] == message_data.keys(), "Invalid comment payload."
+
+        if message_type == 'b':
+            assert ['sms', 'email'] == message_data.keys(),\
+                   "Invalid broadcast payload."
+
+            # TODO: Ensure user is an admin.
+            # TODO: Initiate send.
+
+        message.message = message_data
+
+        return message
+
+    def to_dict(self):
+        """Return a Message entity represented as a dict of values
+        suitable for Message.from_dict.
+        """
+        message = self._default_dict()
+        message["version"] = self.version_
+
+        message['event'] = self.event
+        message['timestamp'] = self.timestamp
+        message['type'] = self.message_type
+        message['message'] = self.message
+
+        return message
+
+
+def scan_groups(groups, message):
+    """Scan over the given set of groups, sending the broadcast to everyone
+    in those groups.
+    """
+    from sosbeacon.utils import insert_tasks
+
+    from sosbeacon.group import Group
+    from sosbeacon.group import ALL_GROUPS_ID
+
+    if len(groups) == 1 and groups[0].id == ALL_GROUPS_ID:
+        groups = Group.query().order(Group.key).iter(keys_only=True)
+
+    tasks = []
+    for group in groups:
+        # TODO: Batch tasks or start
+        tasks.append(get_group_broadcast_task(group, message))
+
+        if len(tasks) > 10:
+            insert_tasks(tasks, GROUP_TX_QUEUE)
+            tasks = []
+
+    if tasks:
+        insert_tasks(tasks, GROUP_TX_QUEUE)
+
+
+def get_group_broadcast_task(group, message, batch_id=''):
+    """Get a task to scan and broadcast messages to all students in group."""
+    group_urlsafe = group.urlsafe()
+    message_urlsafe = message.key.urlsafe()
+
+    name = "tx-%s-%s-%s" % (group_urlsafe, message_urlsafe, batch_id)
+    return taskqueue.Task(
+        url='/task/event/tx/group',
+        name=name,
+        params={
+            'message': message_urlsafe,
+            'group': group_urlsafe,
+            'batch': batch_id
+        }
+    )
+
---
+++ SOSBeacon/test/sosbeacon/event/test_message.py
@@ -0,0 +1,230 @@
+
+import gaetest
+
+from mock import Mock
+
+
+class TestMessageModel(gaetest.TestCase):
+    """Test that Message to / from dict methods work as expected."""
+
+    def test_from_empty_dict(self):
+        """Ensure event key is required to create a message."""
+        from sosbeacon.event.message import Message
+
+        self.assertRaisesRegexp(
+            Exception, 'key is required', Message.from_dict, {})
+
+    def test_from_dict_bad_event_key(self):
+        """Ensure valid event key is required to create message."""
+        from google.appengine.ext import ndb
+
+        from sosbeacon.event.event import Event
+        from sosbeacon.event.message import Message
+
+        event_key = ndb.Key(Event, 1)
+
+        self.assertRaisesRegexp(
+            Exception, "Event not found",
+            Message.from_dict, {'event': event_key})
+
+    def test_from_dict_namespace_mismatch(self):
+        """Ensure event school matches current namespace."""
+        from sosbeacon.event.event import Event
+        from sosbeacon.event.message import Message
+
+        event = Event(school='12345')
+        event_key = Mock()
+        event_key.get.return_value = event
+
+        self.assertRaisesRegexp(
+            Exception, "Security violation",
+            Message.from_dict, {'event': event_key})
+
+    def test_from_dict_comment_payload(self):
+        """Ensure comment message payload matches expected type."""
+        from google.appengine.api import namespace_manager
+
+        from sosbeacon.event.event import Event
+        from sosbeacon.event.message import Message
+
+        event = Event(school=unicode(namespace_manager.get_namespace()))
+        event_key = Mock()
+        event_key.get.return_value = event
+
+        message_dict = {
+            'event': event_key,
+            'type': 'c',
+            'message': {
+                'body': 'A Comment here.'
+            }
+        }
+
+        message = Message.from_dict(message_dict)
+
+        self.assertEqual('c', message.message_type)
+        self.assertEqual(
+            message_dict['message']['body'], message.message['body'])
+
+    def test_from_dict_bad_comment_payload(self):
+        """Ensure comment message payload matches expected type."""
+        from google.appengine.api import namespace_manager
+
+        from sosbeacon.event.event import Event
+        from sosbeacon.event.message import Message
+
+        event = Event(school=unicode(namespace_manager.get_namespace()))
+        event_key = Mock()
+        event_key.get.return_value = event
+
+        message_dict = {
+            'event': event_key,
+            'type': 'c',
+            'message': {
+                'email': 'A Comment here.',
+                'body': 'A Comment here.'
+            }
+        }
+
+        self.assertRaisesRegexp(
+            AssertionError, "Invalid comment payload",
+            Message.from_dict, message_dict)
+
+    def test_from_dict_broadcast_payload(self):
+        """Ensure comment message payload matches expected type."""
+        from google.appengine.api import namespace_manager
+
+        from sosbeacon.event.event import Event
+        from sosbeacon.event.message import Message
+
+        event = Event(school=unicode(namespace_manager.get_namespace()))
+        event_key = Mock()
+        event_key.get.return_value = event
+
+        message_dict = {
+            'event': event_key,
+            'type': 'b',
+            'message': {
+                'sms': 'Your SMS message here.',
+                'email': 'The email message here.'
+            }
+        }
+
+        message = Message.from_dict(message_dict)
+
+        self.assertEqual('b', message.message_type)
+        self.assertEqual(
+            message_dict['message']['sms'], message.message['sms'])
+        self.assertEqual(
+            message_dict['message']['email'], message.message['email'])
+
+    def test_from_dict_bad_broadcast_payload(self):
+        """Ensure comment message payload matches expected type."""
+        from google.appengine.api import namespace_manager
+
+        from sosbeacon.event.event import Event
+        from sosbeacon.event.message import Message
+
+        event = Event(school=unicode(namespace_manager.get_namespace()))
+        event_key = Mock()
+        event_key.get.return_value = event
+
+        message_dict = {
+            'event': event_key,
+            'type': 'b',
+            'message': {
+                'email': 'A Comment here.',
+                'body': 'A Comment here.'
+            }
+        }
+
+        self.assertRaisesRegexp(
+            AssertionError, "Invalid broadcast payload",
+            Message.from_dict, message_dict)
+
+
+class TestScanGroups(gaetest.TestCase):
+    """Test the scan_groups method to ensure it inserts a batch of tasks per
+    ten groups.
+    """
+
+    def test_one_group(self):
+        """Ensure event key is required to create a message."""
+        from sosbeacon import utils
+
+        from sosbeacon.event.message import scan_groups
+
+        insert_tasks_mock = Mock()
+
+        self.patch(utils, 'insert_tasks', insert_tasks_mock)
+
+        group_keys = []
+        for i in range(9):
+            group_key = Mock()
+            group_key.urlsafe.return_value = i + 100
+            group_keys.append(group_key)
+
+        message = Mock()
+        message.key.urlsafe.return_value = 'abc'
+
+        scan_groups(group_keys, message)
+
+        self.assertEqual(insert_tasks_mock.call_count, 1)
+
+    def test_multiple_groups(self):
+        """Ensure event key is required to create a message."""
+        from sosbeacon import utils
+
+        from sosbeacon.event.message import scan_groups
+
+        insert_tasks_mock = Mock()
+
+        self.patch(utils, 'insert_tasks', insert_tasks_mock)
+
+        group_keys = []
+        for i in xrange(35):
+            group_key = Mock()
+            group_key.urlsafe.return_value = i + 100
+            group_keys.append(group_key)
+
+        message = Mock()
+        message.key.urlsafe.return_value = 'abc'
+
+        scan_groups(group_keys, message)
+
+        self.assertEqual(4, insert_tasks_mock.call_count)
+
+        call_history = insert_tasks_mock.call_args_list
+
+        for i in range(3):
+            self.assertEqual(11, len(call_history[i][0][0]))
+
+        self.assertEqual(2, len(call_history[3][0][0]))
+
+    def test_all_groups(self):
+        """Ensure passing in the all groups group, results in a query."""
+        from sosbeacon.event.message import scan_groups
+
+        from sosbeacon.group import Group
+        from sosbeacon.group import ALL_GROUPS_ID
+
+        group_iter_mock = Mock(return_value=[])
+        group_order_mock = Mock(return_value=group_iter_mock)
+        group_query_mock = Mock(return_value=group_order_mock)
+        group_query = Mock(return_value=group_query_mock)
+
+        self.patch(Group, 'query', group_query)
+
+        print ''
+        print group_iter_mock
+        print Group.query().order()
+
+        group_key = Mock()
+        group_key.id = ALL_GROUPS_ID
+
+        message = Mock()
+        message.key.urlsafe.return_value = 'abc'
+
+        scan_groups([group_key], message)
+
+        self.assertEqual(1, group_query_mock.call_count)
+
